c3ff971410df622dae072f90c195edb65f14b368
WeiChien Hsu <weichien711@gmail.com>
2017-11-19 16:32:05 -0600

    add tyepscript sample

---
+++ typeScript/main.ts
@@ -0,0 +1,20 @@
+function log(message){
+    console.log(message);
+}
+
+var message = "Hello World";
+
+log(message);
+
+let a:number;
+let b:boolean;
+let c:string;
+let d:any;
+let e: number[] = [1,2,3]
+let f: any[] = [1, true, "a", false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+
+enum Color {Red = 0, Greem = 1, Blue = 2};
+let backgroundColor = Color.Blue;
