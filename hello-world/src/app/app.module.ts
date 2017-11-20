9feebb030907d8929ad48a3ae520ea2210ec9900
WeiChien Hsu <weichien711@gmail.com>
2017-11-20 13:11:20 -0600

    change element in the HTML page

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoursesComponent } from './courses.component';
 
-
 import { AppComponent } from './app.component';
 
 
