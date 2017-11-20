8514f32739cc20d5a872f18c9f9f8255817c8d1f
WeiChien Hsu <weichien711@gmail.com>
2017-11-20 01:22:16 -0600

    add module

--- typeScript/new.ts
+++ typeScript/new.ts
@@ -1,14 +1,4 @@
-class Point{
-    constructor(private _x?:number, private _y?: number){
-    }
-
-    draw(){
-        console.log("X: " + this._x + "Y: " + this._y);
-    }
-
-   
-    }
-}
+import {Point} from './point';
 
 let point = new Point(1,2);
 point.draw(); 
