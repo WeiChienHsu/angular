ebbaa2b260a76c87c95aa480bd45b9a7d765d3c8
WeiChien Hsu <weichien711@gmail.com>
2017-11-19 20:06:14 -0600

    add arrow function && interface

--- typeScript/main.ts
+++ typeScript/main.ts
@@ -1,19 +1,24 @@
-
-let a:number;
-let b:boolean;
-let c:string;
-let d:any;
-let e: number[] = [1,2,3]
-let f: any[] = [1, true, "a", false];
-
-const ColorRed = 0;
-const ColorGreen = 1;
-
+// Enum
 enum Color {Red = 0, Greem = 1, Blue = 2};
 let backgroundColor = Color.Blue;
 
+// Assertions
 let message;
 message = "abc";
 let endsWithC = (<string>message).endsWith("c");
 console.log(endsWithC);
 
+//Arrow function
+let doLog = (message) => console.log(message);
+
+// Interface
+interface Point {
+    x: number,
+    y: number
+}
+
+let drawPoint = (point:Point) => {
+    //.....
+}
+
+// Class
