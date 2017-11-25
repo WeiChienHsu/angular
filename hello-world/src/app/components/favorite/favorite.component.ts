47a51945713907f93a94c293c2d63e10e7b05bc0
WeiChien Hsu <weichien711@gmail.com>
2017-11-25 00:27:24 -0600

    add Input Component

--- hello-world/src/app/components/favorite/favorite.component.ts
+++ hello-world/src/app/components/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
+  @Input() isFavorite: boolean;
   
   constructor() { }
 
