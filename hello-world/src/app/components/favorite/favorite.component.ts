3e8a5d0e60baa64ea7b849db089807a6838e82e5
WeiChien Hsu <weichien711@gmail.com>
2017-11-25 00:38:01 -0600

    give an alias

--- hello-world/src/app/components/favorite/favorite.component.ts
+++ hello-world/src/app/components/favorite/favorite.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  @Input() isFavorite: boolean;
+  @Input('isFavorite') isFavorite: boolean;
   
   constructor() { }
 
