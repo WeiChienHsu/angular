7d99e99e2a336cd85d8a8eecb900c3678607c0c5
WeiChien Hsu <weichien711@gmail.com>
2017-11-28 00:29:40 -0600

    Add Update

--- http/src/app/component/post/post.component.ts
+++ http/src/app/component/post/post.component.ts
@@ -8,10 +8,29 @@ import { Http } from '@angular/http'
 export class PostComponent {
   posts: any[];
   
-  constructor(http: Http) { 
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  private url = 'http://jsonplaceholder.typicode.com/posts';
+  
+  constructor(private http: Http) { 
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       })
    }
+//Create
+  createPost(input: HTMLInputElement){
+    let post = { title: input.value};
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      })
+  }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+    .subscribe(response => {
+      console.log(response.json());
+    })
+  }
 }
