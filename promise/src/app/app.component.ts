e723ce431c05d38c5fe5c2f5b2629f48d6c63b64
WeiChien Hsu <weichien711@gmail.com>
2017-12-06 12:14:49 -0600

    add promise sample

---
+++ promise/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  ngOnInit(){
+    console.log('setTimeout start execution');
+    setTimeout( ()=> {
+      console.log('callback called');
+    }, 5000);
+    console.log('setTimeout endexecution');
+  }
+}
