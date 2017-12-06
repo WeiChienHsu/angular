cfcd668ef6f620e78c1397c3155c9adc2c686f24
WeiChien Hsu <weichien711@gmail.com>
2017-12-06 16:45:47 -0600

    sync Subject

--- promise/src/app/app.component.ts
+++ promise/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { setTimeout } from 'timers';
+import { Subject } from 'rxjs/Subject';
+
 
 @Component({
   selector: 'app-root',
@@ -12,26 +15,44 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){ 
 
-    let stream$ = new Observable(observer => {
-      console.log(`observable seexution`);
+    // let stream$ = new Observable(observer => {
+    //   console.log(`observable seexution`);
 
-      observer.next(1);
-      observer.next(2);
-      observer.error(new Error('no movie'))
+    //   observer.next(1);
+    //   observer.next(2);
 
+    //   setTimeout( () => {
+    //     observer.next(3);
+    //   }, 3000);
+
+    // });
+
+    // let sub = stream$.subscribe(
+    //   value => alert(value + "!!!!!!!!"),
+    //   error => alert(error),
+    //   () => console.log('done')
+    // );
+
+
+    let subject = new Subject();
+
+    subject.subscribe((v) => {
+      console.log('ObserverA: ' + v);
     });
 
-    let sub = stream$.subscribe(
-      value => alert(value + "!!!!!!!!"),
-      error => alert(error),
-      () => console.log('done')
-    );
+    subject.subscribe((v) => {
+      console.log('ObserverB: ' + v);
+    });
 
-    let sub2 = stream$.subscribe(
-      value => alert(value + "????????"),
-      error => alert(error),
-      () => console.log('done')
-    );
+    subject.next('First Song');
+    subject.next('Second Song');
+
+    subject.subscribe((v) => {
+      console.log('ObserverC: ' + v);
+    });
+
+    subject.next('Third Song');
+    
 
   }
 
